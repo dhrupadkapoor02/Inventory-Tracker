bb033da9345e3c0647193523c510b214191f0e72
dhrupadkapoor02 <dhrupadkapoor02july@gmail.com>
2026-07-09 14:40:07 +0530

    ADD smpt verify

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -3,12 +3,20 @@ const env = require('./env');
 
 const transporter = nodemailer.createTransport({
   host: env.smtp.host,
-  port: env.smtp.port,
-  secure: env.smtp.port === 465,
+  port: Number(env.smtp.port),
+  secure: Number(env.smtp.port) === 465,
   auth: {
     user: env.smtp.user,
     pass: env.smtp.pass,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP server is ready");
+  }
 });
 
 module.exports = transporter;
